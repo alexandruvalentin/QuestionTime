5f4a7a41ecffaba986316327732a4ebe982d4241
Alexandru Grapa <alexandru22valentin@gmail.com>
2022-01-12 08:18:00 +0000

    Create registration templates and custom login redirect urls

--- QuestionTime/QuestionTime/settings.py
+++ QuestionTime/QuestionTime/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'QuestionTime.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "QuestionTime/templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
