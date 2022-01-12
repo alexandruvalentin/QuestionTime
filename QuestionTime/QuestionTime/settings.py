de0b21e34cf3238fed8f523e1bdb8cb17279480b
Alexandru Grapa <alexandru22valentin@gmail.com>
2022-01-12 07:47:40 +0000

    Create custom user model and migrate

--- QuestionTime/QuestionTime/settings.py
+++ QuestionTime/QuestionTime/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
+    'rest_framework.authtoken',
+    
+    'django_registration',
+    'djoser',
+    'widget_tweaks',
+    
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'users.CustomUser'
