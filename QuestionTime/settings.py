b8ffc6a6be01917e97015a885cafedf0945ec660
alexandruvalentin <alexandru22valentin@gmail.com>
2022-02-27 12:02:43 +0000

    Connect Vue CLI and Django development servers together

--- QuestionTime/settings.py
+++ QuestionTime/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'QuestionTime.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "QuestionTime/templates",],
+        'DIRS': [BASE_DIR / "templates",],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
