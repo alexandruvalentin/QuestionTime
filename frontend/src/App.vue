ed72b8f7ff278425e3fcd41923d1241daec820e6
alexandruvalentin <alexandru22valentin@gmail.com>
2022-02-27 16:28:05 +0000

    Add navigation bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>
+    <NavbarComponent />
   </div>
   <router-view />
 </template>
+
+<script>
+import NavbarComponent from "@/components/Navbar.vue"
+export default {
+  name: "App",
+  components: {
+    NavbarComponent
+  }
+}
+</script>
+
 
 <style>
 body {
